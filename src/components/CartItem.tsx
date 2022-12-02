dd6d2fe49a76ce25bfc3892a802ca0eac73ec8b3
Ophélia <ophelia@MacBook-Pro.local>
2022-12-02 09:56:38 +0100

    shoppingCart total set

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "react-bootstrap"
+import { Stack, Button } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import storeItems from "../data/items.json"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type CartItemProps = {
   id: number
@@ -19,14 +20,25 @@ export function CartItem({ id, quantity }: CartItemProps) {
       />
       <div className="me-auto">
         <div>
-          {item.name}{" "}
-          {quantity > 1 && (
+          {item.name}
+          {quantity > 0 && (
             <span className="text-muted" style={{ fontSize: ".65rem" }}>
               x{quantity}
             </span>
           )}
         </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
       </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        x
+      </Button>
     </Stack>
   )
 }
